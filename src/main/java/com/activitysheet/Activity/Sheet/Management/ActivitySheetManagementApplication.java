4a1a7d6b0c8d1c05357caebf56704e4c3fa09222
Deepesh <saraswatdeepesh19@gmail.com>
2022-06-14 17:31:11 +0530

    Initial Setup

---
+++ src/main/java/com/activitysheet/Activity/Sheet/Management/ActivitySheetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.activitysheet.Activity.Sheet.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitySheetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitySheetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activitysheet/Activity/Sheet/Management/ActivitySheetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitysheet.Activity.Sheet.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitySheetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
